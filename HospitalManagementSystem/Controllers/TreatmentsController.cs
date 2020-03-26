73afd9184883c9044a749d58241e0428e99611be
MadhuriHC <madhuric1996@gmail.com>
2020-03-27 04:32:24 +0530

    TreatmentsController.cs, Treatments View added

---
+++ HospitalManagementSystem/Controllers/TreatmentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HospitalManagementSystem.Models;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class TreatmentsController : Controller
+    {
+        private HMSDbContext db = new HMSDbContext();
+
+        // GET: Treatments
+        public ActionResult Index()
+        {
+            var treatments = db.Treatments.Include(t => t.Patient);
+            return View(treatments.ToList());
+        }
+
+        // GET: Treatments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treatment treatment = db.Treatments.Find(id);
+            if (treatment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treatment);
+        }
+
+        // GET: Treatments/Create
+        public ActionResult Create()
+        {
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName");
+            return View();
+        }
+
+        // POST: Treatments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TreatemntID,PatientID,CheckupDate,Symptoms,Diagnosis,Medicine,Doses,BeforeMeal")] Treatment treatment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Treatments.Add(treatment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName", treatment.PatientID);
+            return View(treatment);
+        }
+
+        // GET: Treatments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treatment treatment = db.Treatments.Find(id);
+            if (treatment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName", treatment.PatientID);
+            return View(treatment);
+        }
+
+        // POST: Treatments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TreatemntID,PatientID,CheckupDate,Symptoms,Diagnosis,Medicine,Doses,BeforeMeal")] Treatment treatment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(treatment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "FirstName", treatment.PatientID);
+            return View(treatment);
+        }
+
+        // GET: Treatments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treatment treatment = db.Treatments.Find(id);
+            if (treatment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treatment);
+        }
+
+        // POST: Treatments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Treatment treatment = db.Treatments.Find(id);
+            db.Treatments.Remove(treatment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
